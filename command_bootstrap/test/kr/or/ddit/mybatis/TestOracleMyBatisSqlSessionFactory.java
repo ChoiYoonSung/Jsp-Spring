85b696ddcb799ba0936fb97ff919b19a8ba71f0e
ChoiYoonSung <48468209+ChoiYoonSung@users.noreply.github.com>
2021-05-18 20:51:22 +0900

    21.05.18 - JSP - Mybatis (Session Factory / SqlConfig / Mapper)

---
+++ command_bootstrap/test/kr/or/ddit/mybatis/TestOracleMyBatisSqlSessionFactory.java
@@ -0,0 +1,37 @@
+package kr.or.ddit.mybatis;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import kr.or.ddit.dto.MenuVO;
+
+public class TestOracleMyBatisSqlSessionFactory {
+
+	private SqlSessionFactory factory = new OracleMyBatisSessionFactory();
+	private SqlSession session;
+
+	@Before
+	public void openSession() {
+		session = factory.openSession();
+	}
+
+	@Test
+	public void testSqlSession() {
+		Assert.assertNotNull(session.getConnection());
+	}
+
+	@Test
+	public void testSql() {
+		MenuVO menu = session.selectOne("Menu-Mapper.selectMenuByMcode", "M010100");
+		Assert.assertEquals("회원목록", menu.getMname());
+	}
+
+	@After
+	public void closeSession() {
+		session.close();
+	}
+}
