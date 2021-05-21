6ec75d1d439ff0008589bf99fb3e99fc7708a696
ChoiYoonSung <48468209+ChoiYoonSung@users.noreply.github.com>
2021-05-21 21:34:10 +0900

    21.05.21 - JSP - ApplicationContext / Listener

--- command_bootstrap/src/kr/or/ddit/service/MockMenuDAO.java
+++ command_bootstrap/src/kr/or/ddit/service/MockMenuDAO.java
@@ -1,6 +1,7 @@
 package kr.or.ddit.service;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -13,8 +14,13 @@ public class MockMenuDAO implements MenuDAO{
 	@Override
 	public List<MenuVO> selectMainMenu(SqlSession session) throws SQLException {
 		List<MenuVO> menuList = null;
+		
+		if(session.getConnection() == null) throw new SQLException();
+		
 		MenuVO menu = new MenuVO();
 		menu.setMname("회원가입");
+		
+		menuList = new ArrayList<MenuVO>();
 		menuList.add(menu);
 		
 		return menuList;
@@ -23,13 +29,32 @@ public class MockMenuDAO implements MenuDAO{
 	@Override
 	public List<MenuVO> selectSubMenu(SqlSession session, String mCode) throws SQLException {
 		List<MenuVO> menuList = null;
+		
+		if(session.getConnection() == null) throw new SQLException();
+		
+		MenuVO menu = new MenuVO();
+		menu.setMname("회원가입");
+		
+		menuList = new ArrayList<MenuVO>();
+		menuList.add(menu);
+		
+		if(!mCode.equals("M010000")) menuList = null;
+		
 		return menuList;
 	}
 
 	@Override
 	public MenuVO selectMenuByMcode(SqlSession session, String mCode) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		MenuVO menu = null;
+		
+		if(session.getConnection() == null) throw new SQLException();
+		
+		if(mCode.equals("M010000")) {
+			menu = new MenuVO();
+			menu.setMname("회원가입");
+		}
+		
+		return menu;
 	}
 
 }
